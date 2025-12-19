235b8cda4d275ba26d3e9bd2a7024e5db52b07f7
mohammed123-code <afsalsandhu@gmail.com>
2025-12-19 08:47:30 +0530

    Made changes

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,10 @@ const authRoutes = require("./routes/authRoutes");
 const app = express();
 
 // middleware
-app.use(cors());
+app.use(cors({
+    origin: "https://13.60.43.61:3000",
+    credentials: true
+}));
 app.use(express.json());
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
